a3efe0f0c9396b4b16ce7744d43affba19c71548
Shiboka <d.aubie@outlook.com>
2024-05-22 21:29:14 -0300

    removed unnecessary code

--- backend/app/models/board.rb
+++ backend/app/models/board.rb
@@ -3,16 +3,4 @@ class Board < ApplicationRecord
   validates :name, presence: true
 
   has_many :threds, dependent: :destroy
-
-  def serialize
-    board_hash = { id:, name:, path:, threds: [] }
-
-    threds.each do |t|
-      board_hash[:threds].push(t.preview)
-    end
-
-    board_hash[:threds].sort_by! { |t| t[:posts].last.created_at }.reverse!
-
-    board_hash
-  end
 end
